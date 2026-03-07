b947ae3c084610e0c1b7ef8b3e5830ba769b1efd
yarn-code-reader <qurrratulain2411@gmail.com>
2026-03-07 08:03:07 +0500

    Initial Project Structure

---
+++ input/fileReading.py
@@ -0,0 +1,6 @@
+import csv
+import path as p
+
+
+def read_config_file(pathOfFile): #takes path as param and returns config structure
+    
---
+++ path.py
@@ -0,0 +1,6 @@
+import os
+
+base_path = os.path.dirname(__file__)
+
+def return_base_directory():
+    return base_path
